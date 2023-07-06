0524575a11e7dc9b0794700567291867abf3bef0
nian23sat <anicosat@gmail.com>
2023-07-06 01:45:19 -0300

    Inicio del proyecto

---
+++ steps-venv.py
@@ -0,0 +1,62 @@
+# entorno virtual es solo en python
+#
+#python --version
+#pip --version
+
+# BUSCAR INSTALAR PIP SI NO ESTA INSTALADO
+# COMPROBAR VENV O INSTALAR VENV
+
+#pipp list
+#pip freeze
+
+#python -m venv nombre-proyecto
+
+# python -m  :  m de modulo
+
+
+# ACTIVAR ENTORNO VIRTUAL
+
+# venv - Scripts - ./activate
+# Scripts - ./deactivate
+
+# Inicializar git
+
+# git init
+
+# crea el repositorio vacio
+# si no esta instalado git, instalar desde la pagina oficiAL
+
+# INSTALAR FLASK
+
+# pip install flask
+
+# instalar - base de datos
+
+# pip install Flask-MySQL
+
+# instalar DB con credenciales
+
+# pip install cryptography
+
+
+# volcar dependencias en un txt para compartir en un repositorio
+# y las personas que descarguen el repo, sepan que necesitan tener
+# instalado
+
+# el comando para crear el txt con dependencias es:
+# pip freeze > requirements.txt
+
+# puede tener otro nombre, por convencion es requirements.txt
+
+
+# comando para instalar las dependencias hechas anteriormente
+
+# pip install -r requirements.txt
+
+
+# CREAR un Readme
+
+# README.md
+
+
+

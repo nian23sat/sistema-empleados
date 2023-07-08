44865c085f9bf21ef32b600abe66c0dae227ab02
nian23sat <anicosat@gmail.com>
2023-07-08 20:08:16 -0300

    Haciendo commit 8-7-23

--- steps-venv.py
+++ steps-venv.py
@@ -79,6 +79,37 @@
 # git commit -m "inicio del proyecto"
 
 
+# crear repositorio nuevo en github
+# opcion de repositorio existente
 
+"""
+…or push an existing repository from the command line
+git remote add origin https://github.com/nian23sat/sistema-empleados.git
+git branch -M main
+git push -u origin main
+"""
+
+# si el ultimo comando lanza error debido a otra cuenta de git
+# reconfigurar credenciales
+
+# git config credential.username "usuario"
+
+# se le pedira autenticar el usuario pidiendo password
+
+
+# AGREGAR GIT IGNORE
+# .GITIGNORE
+
+# git push
+# envia los commits a github
+
+
+# Crear rama de trabajo para NO trabajar sobre main
+# que es la rama principal y suele ser de produccion
+
+# git switch -c develop
+
+# crear otra rama mas
+# git switch -c 01-instancia-flask
 
 
